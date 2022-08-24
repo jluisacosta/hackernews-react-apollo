8093e3e8bf3138bf6b40ca206141cf8eabb85b60
jluisacosta <jluis.acosta@outlook.com>
2022-08-24 12:16:40 -0500

    Implement cache handling for links and votes mutations

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,61 @@
 import React from 'react';
+import { FEED_QUERY } from './LinkList';
 import { AUTH_TOKEN } from '../constants';
 import { timeDifferenceForDate } from '../utils';
+import { useMutation, gql } from '@apollo/client';
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        id
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
 
 const Link = (props) => {
   const { link } = props;
   const authToken = localStorage.getItem(AUTH_TOKEN);
+  const [vote] = useMutation(VOTE_MUTATION, {
+    variables: {
+      linkId: link.id
+    },
+    update: (cache, {data: {vote}}) => {
+      const { feed } = cache.readQuery({
+        query: FEED_QUERY
+      });
+
+      const updatedLinks = feed.links.map((feedLink) => {
+        if (feedLink.id === link.id) {
+          return {
+            ...feedLink,
+            votes: [...feedLink.votes, vote]
+          };
+        }
+        return feedLink;
+      });
+
+      cache.writeQuery({
+        query: FEED_QUERY,
+        data: {
+          feed: {
+            links: updatedLinks
+          }
+        }
+      });
+    }
+  });
 
   return (
     <div className="flex mt2 items-start">
@@ -14,7 +65,7 @@ const Link = (props) => {
           <div
             className="ml1 gray f11"
             style={{ cursor: 'pointer' }}
-            onClick={() => {console.log("Clicked vote button")}}
+            onClick={vote}
           >
             ▲
           </div>
